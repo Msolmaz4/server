eabd94410021678273e9eb41eabaeb0cadb6ad2f
muhammet solmnaz <msolmaz83@gmail.com>
2021-07-01 17:44:18 +0200

    hata FAlsch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express(); 
 
+app.use(bodyParser.urlencuded({ extended: false}))
+app.use(bodyParser.json())
+
 app.get('/', (req,res) => {
 
-    res.send('REST API7'); 
+      const sonuc = {
+       durum = true,
+      mesaj = "tamamlandi"
+        }; 
 
+    res.send('sonuc'); 
 });
+
+app.post('/',(req,rest) => {
+
+ res.send({durum : true});
+});
+
 app.listen(3300, () => console.log("calisti"))
  
